76e17908f3e8b835ee781647eb8d852eaddc510a
Travis Exline <t.exline22@gmail.com>
2019-08-27 17:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function mapToNegative(sourceArray) {
   let numArray = []
   for (let i = 0; i < sourceArray.length; i++) {
-    numArray.push(-1 * i)
+    numArray.push(-1 * sourceArray[i])
   }
 }
