d58aa9b1e385bef7dd9881f0ba376fc6af6f30d8
Travis Exline <t.exline22@gmail.com>
2019-08-27 17:56:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,3 +44,10 @@ function reduceToAllTrue(sourceArray) {
   }
   return true
 }
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) return true
+  }
+  return false
+}
