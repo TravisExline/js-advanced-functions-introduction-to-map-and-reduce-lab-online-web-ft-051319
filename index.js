0319927af2e3eff5b85a3c8e4d38f83970058251
Travis Exline <t.exline22@gmail.com>
2019-08-27 17:55:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function mapToSquare(sourceArray) {
   for (let i = 0; i < sourceArray.length; i++) {
     numArray.push(sourceArray[i] * sourceArray[i])
   }
+  return numArray
 }
 
 function reduceToTotal(sourceArray, startingPoint=0) {
